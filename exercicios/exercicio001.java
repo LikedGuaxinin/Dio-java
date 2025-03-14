632347824ba81a607f53ab11d5467177efd6f542
LikedGuaxinin <diegomarquesconstantino@gmail.com>
2025-03-14 20:09:36 -0300

    commit1

---
+++ exercicios/exercicio001.java
@@ -0,0 +1,8 @@
+public class exercicio001{
+
+public static void main(String[] args) {
+    System.out.println("Diego");
+}
+
+
+}
---
+++ tipos-e-variaveis/src/main/java/tipos-e-variaveis/TiposEVariaveis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tipos-e-variaveis;
+
+/**
+ *
+ * @author diego
+ */
+public class TiposEVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
